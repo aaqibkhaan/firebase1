09fd1a7fad30ef4298c2700edbb5ecff7f4ec153
aaqibkhaan <aaqibrashid@hotmail.com>
2017-11-01 14:50:25 +0000

    All basic structure and modules added

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Usurvey from './Usurvey';
+
+
+class App extends Component {
+
+
+	render() {
+
+		return(
+			<div>
+			Welcome to FIREBASE
+			<Usurvey />
+			</div>
+			);
+	}
+}
+
+
+export default App;
---
+++ src/components/Usurvey.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Usurvey extends Component {
+
+
+	render() {
+
+		return (
+			<div>
+			You are inside Usurvey
+			</div>
+			);
+	}
+}
+
+
+export default Usurvey;
